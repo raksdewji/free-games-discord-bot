e5eb844500b86d2cbae6216faed3abfe52da6d12
raksdewji <rakheemdewji@hotmail.com>
2021-01-07 18:49:37 -0700

    feat: basic implmentation of sending free games to channel

--- bot.js
+++ bot.js
@@ -1,4 +1,6 @@
 const Discord = require('discord.js');
+const redditFetch = require('tsumiki-reddit.js');
+
 require('dotenv').config();
 
 const client = new Discord.Client();
@@ -24,4 +26,55 @@ client.on('message', function (msg, err) {
   if (msg.content === 'ping' && msg.channel.id === '791793487318220808') {
     msg.channel.send('pong');
   }
+
+  // Send free games when users types !user
+  if (msg.content === '!free') {
+    fetchFreeGames();
+  }
 });
+
+async function fetchFreeGames() {
+  await redditFetch({
+    subreddit: 'GameDeals',
+    type: 'hot',
+    allowNSFW: true,
+    allowModPost: true,
+    allowCrossPost: true,
+    allowAllDomains: true,
+    amount: 20
+  }).then(post => {
+    if (post === null) {
+      return console.log('Check your options!\nAnd check if you spelled the subreddit correctly.');
+    }
+
+    if (post === 0) {
+      return console.log('Could not find any posts to fetch from that subreddit! Try another one!');
+    }
+
+    const channel = client.channels.cache.get('791793487318220808');
+
+    for (let i = 0; i < 20; i++) {
+      if (post[i].data.title.includes('free') ||
+          post[i].data.title.includes('Free') ||
+          post[i].data.title.includes('100%')) {
+        console.log(post[i].data);
+        const embedMsg = new Discord.MessageEmbed()
+          .setTitle(post[i].data.title)
+          .setURL(`https://www.reddit.com${post[i]?.data?.permalink}`)
+          .setColor('#0099ff')
+          .setDescription(`Free game here: ${post[i]?.data?.url}`);
+
+        if (post[i]?.data?.thumbnail !== 'spoiler' && post[i]?.data?.thumbnail !== 'default') {
+          embedMsg.setImage(`${post[i]?.data?.thumbnail}`);
+        }
+        channel.send(embedMsg);
+      }
+    }
+  }).catch((err) => {
+    console.log('error caught: ', err);
+  });
+};
+
+// setTimeout(function() {
+//   fetchHotPosts();
+// }, 1000);
