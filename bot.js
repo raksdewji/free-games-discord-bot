d0ce19b5308caaedfa15b0b4d9dcc45196207627
raksdewji <rakheemdewji@hotmail.com>
2021-01-10 00:17:52 -0700

    feat: node fetch, help commands, error checking

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const redditFetch = require('tsumiki-reddit.js');
+const nodeFetch = require('node-fetch');
 
 require('dotenv').config();
 
@@ -14,89 +14,132 @@ client.on('ready', function (err, token) {
     console.log(err);
   }
   console.log('Bot online');
+
+  client.user.setActivity('fgHelp', { type: 'LISTENING' });
 });
 
 client.on('message', function (msg, err) {
   if (err) {
     console.log(err);
   }
-  console.log(msg.content);
 
-  if (msg.content === 'test') {
-    msg.channel.send('test works');
-  }
-
-  if (msg.content === 'fetch') {
-    msg.channel.send('fetching');
-    fetchGames();
-  }
-
-  if (msg.content === 'ping') {
-    msg.channel.send('pong');
-  }
-
-  if (msg.content === '!addFree') {
+  // Add the channel to list of channels which free games are sent
+  if (msg.content === 'fgAdd') {
     channelsList.push(msg.channel.id);
-    msg.channel.send('Free game alerts will now be sent here.');
+    msg.channel.send('Free game alerts will now be sent to this channel.');
   }
 
-  if (msg.content === '!removeFree') {
+  // Removes the channel to list of channels which free games are sent
+  if (msg.content === 'fgRemove') {
     const i = channelsList.indexOf(msg.channel.id);
     if (i !== -1) {
       channelsList.splice(i, 1);
     }
-    msg.channel.send('No longer receiving free game alerts');
+    msg.channel.send('No longer receiving free game alerts here.');
   }
 
-  // Send free games when users types !free
-  if (msg.content === '!free') {
+  // Send free games when users types sendFreeGames
+  if (msg.content === 'fgSend') {
     getCurrentGames(msg.channel.id);
+  }
+
+  if (msg.content === 'fgHelp') {
+    helpInfo(msg.channel.id);
+  }
+
+  if (msg.content === 'fgActive') {
+    if (channelsList.indexOf(msg.channel.id) !== -1) {
+      msg.channel.send('Currently receiving free game alerts here');
+    } else {
+      msg.channel.send('Not receiving free game alerts here');
+    }
   }
 });
 
+function helpInfo (channelID) {
+  const clientChannel = client.channels.cache.get(channelID);
+
+  const embedMsg = new Discord.MessageEmbed()
+    .setColor('#0099ff')
+    .setTitle('Commands')
+    .addFields(
+      {
+        name: 'fgAdd',
+        value: 'Receive free game alerts to current channel',
+        inline: true
+      },
+      {
+        name: 'fgRemove',
+        value: 'Unsubscribe from free game alerts to this channel',
+        inline: true
+      },
+      {
+        name: 'fgActive',
+        value: 'Check if current channel is receiving free game alerts',
+        inline: true
+      }
+    )
+    .setAuthor('Free Games Bot',
+      'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png',
+      'https://github.com/raksdewji/free-games-discord-bot');
+
+  clientChannel.send(embedMsg);
+}
+
 const getCurrentGames = async (channel) => {
-  await redditFetch({
-    subreddit: 'GameDeals',
-    type: 'hot',
-    allowNSFW: true,
-    allowModPost: true,
-    allowCrossPost: true,
-    allowAllDomains: true,
-    amount: 20
-  }).then(post => {
-    if (post === null) {
-      return console.log('Check your options!\nAnd check if you spelled the subreddit correctly.');
+  const clientChannel = client.channels.cache.get(channel);
+  const targetURL = 'https://reddit.com/r/gamedeals/hot.json?sort=hot&t=day&limit=100';
+
+  await nodeFetch(targetURL).then(res => res.json()).then(body => {
+    let post;
+
+    if (!body.data) {
+      console.log('No posts found');
+      post = null;
+      return post;
+    } else if (!body.data.children || body.data.children <= 0) {
+      console.log('Invalid');
+      post = 'invalid';
+      return post;
     }
 
-    if (post === 0) {
-      return console.log('Could not find any posts to fetch from that subreddit! Try another one!');
-    }
-
-    const clientChannel = client.channels.cache.get(channel);
+    post = body.data.children;
+    post = post.slice(0, 20); // Get top 20 hot posts
 
     for (let i = 0; i < 20; i++) {
       if (post[i].data.title.includes('free') ||
-          post[i].data.title.includes('Free') ||
-          post[i].data.title.includes('100%')) {
+            post[i].data.title.includes('Free') ||
+            post[i].data.title.includes('100%')) {
         console.log(post[i].data);
-        const embedMsg = new Discord.MessageEmbed()
-          .setTitle(post[i].data.title)
-          .setURL(`https://www.reddit.com${post[i]?.data?.permalink}`)
-          .setColor('#0099ff')
-          .setDescription(`Free game here: ${post[i]?.data?.url}`);
 
-        if (post[i]?.data?.thumbnail !== 'spoiler' && post[i]?.data?.thumbnail !== 'default') {
-          embedMsg.setImage(`${post[i]?.data?.thumbnail}`);
+        let title = post[i]?.data?.title;
+        if (title.length > 256) {
+          title = title.substring(0, 256);
         }
-        console.log(`${post[i]?.data?.permalink}${clientChannel}`);
+
+        let thumbnail;
+        if (post[i]?.data?.thumbnail !== 'spoiler' && post[i]?.data?.thumbnail !== 'default') {
+          thumbnail = post[i]?.data?.thumbnail;
+        }
+
+        const embedMsg = new Discord.MessageEmbed()
+          .setColor('#0099ff')
+          .setTitle(title)
+          .setURL(`https://www.reddit.com${post[i]?.data?.permalink}`)
+          .setDescription(`Free game here: ${post[i]?.data?.url}`)
+          .setAuthor('Free Games Bot',
+            'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png',
+            'https://github.com/raksdewji/free-games-discord-bot')
+          .setImage(thumbnail);
+
+        console.log('Sent game: ', `${post[i]?.data?.permalink}${clientChannel}`);
+
         if (sentGames.indexOf(`${post[i]?.data?.permalink}${clientChannel}`) === -1) {
           sentGames.push(`${post[i]?.data?.permalink}${clientChannel}`);
           clientChannel.send(embedMsg);
         }
       }
     }
-  }).catch((err) => {
-    console.log('error caught: ', err);
   });
 };
 
@@ -104,9 +147,13 @@ const fetchGames = async () => {
   channelsList.forEach(getCurrentGames);
 };
 
-// Call fetchGames every 30 min (1800000)
+// Call fetchGames every 30 min (1800000 milliseconds)
 setInterval(function () {
   fetchGames();
   console.log('30 minute timer');
-  sentGames.splice(0, sentGames.length - 1000); // length of sent games array under 1000
+
+  if (sentGames.length > 100000) {
+    sentGames.splice(0, sentGames.length - 50000); // remove half of sent games list
+    console.log('cleaned array');
+  }
 }, 1800000);
