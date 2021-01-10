6fe2c3b448d7abed4b866d0da0d97e8697e9d51f
raksdewji <rakheemdewji@hotmail.com>
2021-01-09 19:49:54 -0700

    feat: fetch function to periodically check new posts
    - also checks to make sure when fetching and sending that duplicate posts arent sent

--- bot.js
+++ bot.js
@@ -6,6 +6,9 @@ require('dotenv').config();
 const client = new Discord.Client();
 client.login(process.env.bot_token);
 
+const channelsList = [];
+const sentGames = [];
+
 client.on('ready', function (err, token) {
   if (err) {
     console.log(err);
@@ -19,21 +22,39 @@ client.on('message', function (msg, err) {
   }
   console.log(msg.content);
 
-  if (msg.content === 'test' && msg.channel.id === '791793487318220808') {
-    msg.reply('test works');
+  if (msg.content === 'test') {
+    msg.channel.send('test works');
   }
 
-  if (msg.content === 'ping' && msg.channel.id === '791793487318220808') {
+  if (msg.content === 'fetch') {
+    msg.channel.send('fetching');
+    fetchGames();
+  }
+
+  if (msg.content === 'ping') {
     msg.channel.send('pong');
   }
 
-  // Send free games when users types !user
+  if (msg.content === '!addFree') {
+    channelsList.push(msg.channel.id);
+    msg.channel.send('Free game alerts will now be sent here.');
+  }
+
+  if (msg.content === '!removeFree') {
+    const i = channelsList.indexOf(msg.channel.id);
+    if (i !== -1) {
+      channelsList.splice(i, 1);
+    }
+    msg.channel.send('No longer receiving free game alerts');
+  }
+
+  // Send free games when users types !free
   if (msg.content === '!free') {
-    fetchFreeGames(msg.channel.id);
+    getCurrentGames(msg.channel.id);
   }
 });
 
-const fetchFreeGames = async (channel) => {
+const getCurrentGames = async (channel) => {
   await redditFetch({
     subreddit: 'GameDeals',
     type: 'hot',
@@ -67,7 +88,11 @@ const fetchFreeGames = async (channel) => {
         if (post[i]?.data?.thumbnail !== 'spoiler' && post[i]?.data?.thumbnail !== 'default') {
           embedMsg.setImage(`${post[i]?.data?.thumbnail}`);
         }
-        clientChannel.send(embedMsg);
+        console.log(`${post[i]?.data?.permalink}${clientChannel}`);
+        if (sentGames.indexOf(`${post[i]?.data?.permalink}${clientChannel}`) === -1) {
+          sentGames.push(`${post[i]?.data?.permalink}${clientChannel}`);
+          clientChannel.send(embedMsg);
+        }
       }
     }
   }).catch((err) => {
@@ -75,6 +100,13 @@ const fetchFreeGames = async (channel) => {
   });
 };
 
-// setTimeout(function() {
-//   fetchHotPosts();
-// }, 1000);
+const fetchGames = async () => {
+  channelsList.forEach(getCurrentGames);
+};
+
+// Call fetchGames every 30 min (1800000)
+setInterval(function () {
+  fetchGames();
+  console.log('30 minute timer');
+  sentGames.splice(0, sentGames.length - 1000); // length of sent games array under 1000
+}, 1800000);
