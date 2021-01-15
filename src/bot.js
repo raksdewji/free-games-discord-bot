fd3400d3a3ffe1cca3a324bb2f703888272219aa
raksdewji <rakheemdewji@hotmail.com>
2021-01-15 11:16:56 -0700

    refactor: fetch posts once to reduce complexity

--- src/bot.js
+++ src/bot.js
@@ -41,9 +41,9 @@ client.on('message', function (msg, err) {
     console.log('Removed:', msg.channel.id);
   }
 
-  // Send free games when users types sendFreeGames
+  // Send free games when users types sendFreeGames, won't send duplicates
   if (msg.content === 'fgSend') {
-    getCurrentGames(msg.channel.id);
+    sendGames(msg.channel.id);
   }
 
   if (msg.content === 'fgHelp') {
@@ -89,77 +89,90 @@ function helpInfo (channelID) {
   clientChannel.send(embedMsg);
 }
 
-const getCurrentGames = async (channel) => {
-  const clientChannel = client.channels.cache.get(channel);
+const sendGames = async (channelID) => {
+  const posts = await fetchPosts();
+  if (posts !== 'invalid' && posts !== null) {
+    getCurrentGames(channelID, posts);
+  }
+};
+
+// Fetch posts from Reddit using node-fetch and return the body
+const fetchPosts = async () => {
   const targetURL = 'https://reddit.com/r/gamedeals/new.json?sort=new&t=week&limit=100';
 
-  await nodeFetch(targetURL).then(res => res.json()).then(body => {
-    let post;
-
+  try {
+    const res = await nodeFetch(targetURL);
+    const body = await res.json();
     if (!body.data) {
       console.log('No posts found');
-      post = null;
-      return post;
+      return null;
     } else if (!body.data.children || body.data.children <= 0) {
-      console.log('Invalid');
-      post = 'invalid';
-      return post;
+      console.log('invalid');
+      return 'invalid';
     }
+    return body;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-    post = body.data.children;
+const getCurrentGames = async (channel, post) => {
+  const clientChannel = client.channels.cache.get(channel);
+  post = post.data.children;
 
-    for (let i = 0; i < 60; i++) {
-      if (post[i].data.title.includes('free') ||
+  for (let i = 0; i < 60; i++) {
+    if (post[i].data.title.includes('free') ||
           post[i].data.title.includes('Free') ||
           post[i].data.title.includes('100%')) {
-        if (post[i].data.ups > 200 && post[i].data.thumbnail !== 'spoiler') { // posts with > 200 scores and not expired
-          let title = post[i].data.title;
-          if (title.length > 256) {
-            title = title.substring(0, 256);
-          }
+      if (post[i].data.ups > 200 && post[i].data.thumbnail !== 'spoiler') { // posts with > 200 scores and not expired
+        let title = post[i].data.title;
+        if (title.length > 256) {
+          title = title.substring(0, 256);
+        }
 
-          let thumbnail;
-          if (post[i].data.thumbnail === 'default') { // no thumbnail from reddit
-            thumbnail = 'https://www.reddit.com/static/noimage.png';
-          } else {
-            thumbnail = post[i].data.thumbnail;
-          }
+        let thumbnail;
+        if (post[i].data.thumbnail === 'default') { // no thumbnail from reddit
+          thumbnail = 'https://www.reddit.com/static/noimage.png';
+        } else {
+          thumbnail = post[i].data.thumbnail;
+        }
 
-          const embedMsg = new Discord.MessageEmbed()
-            .setColor('#0099ff')
-            .setTitle(title)
-            .setURL(`https://www.reddit.com${post[i].data.permalink}`)
-            .setDescription(`Free game here: ${post[i].data.url}`)
-            .setAuthor('Free Games Bot',
-              'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png',
-              'https://github.com/raksdewji/free-games-discord-bot')
-            .setImage(thumbnail);
+        const embedMsg = new Discord.MessageEmbed()
+          .setColor('#0099ff')
+          .setTitle(title)
+          .setURL(`https://www.reddit.com${post[i].data.permalink}`)
+          .setDescription(`Free game here: ${post[i].data.url}`)
+          .setAuthor('Free Games Bot',
+            'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png',
+            'https://github.com/raksdewji/free-games-discord-bot')
+          .setImage(thumbnail);
 
+        if (sentGames.indexOf(`${post[i].data.permalink}${clientChannel}`) === -1) {
+          sentGames.push(`${post[i].data.permalink}${clientChannel}`);
+          clientChannel.send(embedMsg);
           console.log('Sent game: ', `${post[i].data.permalink}${clientChannel}`);
-
-          if (sentGames.indexOf(`${post[i].data.permalink}${clientChannel}`) === -1) {
-            sentGames.push(`${post[i].data.permalink}${clientChannel}`);
-            clientChannel.send(embedMsg);
-          }
         }
       }
     }
-  });
+  }
 };
 
 const fetchGames = async () => {
-  channelsList.forEach(getCurrentGames);
+  const posts = await fetchPosts();
+  if (posts !== 'invalid' && posts !== null) {
+    channelsList.forEach(async function (channel) {
+      getCurrentGames(channel, posts);
+    });
+  }
 };
 
-// Call fetchGames every 30 min (1800000 milliseconds)
+// Call fetchGames every 1 hour (3600000 milliseconds)
 setInterval(function () {
   fetchGames();
-  console.log('30 minute timer');
+  console.log('1 hour timer');
 
-  if (sentGames.length > 100000) {
-    sentGames.splice(0, sentGames.length - 50000); // remove half of sent games list
+  if (sentGames.length > 1000000) {
+    sentGames.splice(0, sentGames.length - 250000); // remove quarter of sent games list
     console.log('cleaned array');
   }
-}, 1800000);
-
-// Add channel to Postgres DB
+}, 3600000);
